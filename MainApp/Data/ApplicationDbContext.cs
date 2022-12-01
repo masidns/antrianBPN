94b7188c1c84f2345c50cbf2f8af68f5b71adcad
masidns <masidns@gmail.com>
2022-12-02 00:54:22 +0900

    Add project files.

---
+++ MainApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MainApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace MainApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blog { get; set; }
+
+
+    }
+}
